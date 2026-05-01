5fa83409c061650c04e3289132da04be1dc2d1f8
Aditya Meshram <adityameshram623@gmail.com>
2026-05-01 13:00:55 +0530

    recking jenkins connection

--- pages/server.js
+++ pages/server.js
@@ -1,5 +1,12 @@
-<div>
-    <p>Server Page</p>
-    <h1>Welcome to the Server Page</h1>
-    <p>This page is under construction. Please check back later for updates.</p>
-</div>
+<>
+    <div>
+        <p>Server Page</p>
+        <h1>Welcome to the Server Page</h1>
+        <p>This page is under construction. Please check back later for updates.</p>
+    </div>
+    <div>
+        <p>Server Page</p>
+        <h1>Welcome to the Server Page</h1>
+        <p>This page is under construction. Please check back later for updates.</p>
+    </div>
+</>
